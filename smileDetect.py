54821fd5b93d608c0210c609cdb7a9720d0467f9
zhecan wang <zhecanwang@zhecans-mbp.olin.edu>
2016-11-02 21:39:46 -0400

    smile detect prototyping

---
+++ smileDetect.py
@@ -0,0 +1,105 @@
+from sklearn.externals import joblib
+import cv2
+from scipy.ndimage import zoom
+import numpy as np
+
+svc_1 = joblib.load('./data/faceDetectSVC.pkl')
+
+
+def detect_face(frame):
+    cascPath = './lib/haarcascade_frontalface_alt.xml'
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    detected_faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=6,
+            minSize=(100, 100),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+    return gray, detected_faces
+
+
+def extract_face_features(gray, detected_face, offset_coefficients):
+    (x, y, w, h) = detected_face
+    horizontal_offset = offset_coefficients[0] * w
+    vertical_offset = offset_coefficients[1] * h
+    extracted_face = gray[y+vertical_offset:y+h, 
+                      x+horizontal_offset:x-horizontal_offset+w]
+    new_extracted_face = zoom(extracted_face, (64. / extracted_face.shape[0], 
+                                           64. / extracted_face.shape[1]))
+    new_extracted_face = new_extracted_face.astype(np.float32)
+    new_extracted_face /= float(new_extracted_face.max())
+    return new_extracted_face
+
+
+def predict_face_is_smiling(extracted_face):
+    return svc_1.predict(extracted_face.ravel())
+
+
+def make_map(facefile):
+    c1_range = linspace(0, 0.35)
+    c2_range = linspace(0, 0.3)
+    result_matrix = nan * zeros_like(c1_range * c2_range[:, newaxis])
+    gray, detected_faces = detect_face(cv2.imread(facefile))
+    for face in detected_faces[:1]:
+        for ind1, c1 in enumerate(c1_range):
+            for ind2, c2 in enumerate(c2_range):
+                extracted_face = extract_face_features(gray, face, (c1, c2))
+                result_matrix[ind1, ind2] = predict_face_is_smiling(extracted_face)
+    return (c1_range, c2_range, result_matrix)
+
+
+
+
+video_capture = cv2.VideoCapture(0)
+
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    # detect faces
+    gray, detected_faces = detect_face(frame)
+    print "detected_faces ", detected_faces
+
+    face_index = 0
+    
+    # predict output
+    for face in detected_faces:
+        print 7
+        (x, y, w, h) = face
+        if w > 100:
+            print 8
+            # draw rectangle around face 
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            
+            # extract features
+            extracted_face = extract_face_features(gray, face, (0.03, 0.05)) #(0.075, 0.05)
+
+            # predict smile
+            prediction_result = predict_face_is_smiling(extracted_face)
+            print "prediction_result, ",  prediction_result
+
+            # draw extracted face in the top right corner
+            frame[face_index * 64: (face_index + 1) * 64, -65:-1, :] = cv2.cvtColor(extracted_face * 255, cv2.COLOR_GRAY2RGB)
+
+            # annotate main image with a label
+            if prediction_result == 1:
+                cv2.putText(frame, "SMILING",(x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, 155, 10)
+            else:
+                cv2.putText(frame, "not smiling",(x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, 155, 10)
+
+            # increment counter
+            face_index += 1
+                
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
